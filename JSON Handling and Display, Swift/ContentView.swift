968aec3deb02afc45718fd5c99070b3eb03d9ece
Ian Brown <ibapps39@gmail.com>
2021-05-02 18:51:21 -0600

    Initial Commit

---
+++ JSON Handling and Display, Swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JSON Handling and Display, Swift
+//
+//  Created by Ian Brown on 5/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JSON Handling and Display, Swift/JSON_Handling_and_Display__SwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  JSON_Handling_and_Display__SwiftApp.swift
+//  JSON Handling and Display, Swift
+//
+//  Created by Ian Brown on 5/2/21.
+//
+
+import SwiftUI
+
+@main
+struct JSON_Handling_and_Display__SwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
